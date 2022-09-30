318f79def54ca2bf27cefbeb0ffa92d71beda960
yukiice <58852690+yukiice@users.noreply.github.com>
2022-09-30 21:36:47 +0800

    变量和常量

---
+++ helloworld/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");  //这是第一次写java的注释
+        System.out.println("this is yukiice");
+        int a,b;
+        a = 20;
+        b = 30;
+        System.out.println(a);
+        System.out.println(b);
+        final int c = 100;   // final 会把一个变量变为一个常量  只有第一次可以修改，后续不可修改
+        System.out.println(c);
+    }
+}
