f98d2560a29591fc9e70ea053e1cc839efdb975a
yukiice <58852690+yukiice@users.noreply.github.com>
2022-10-13 21:11:15 +0800

    复习的东西

---
+++ OOPSenior/src/com/test/Child.java
@@ -0,0 +1,8 @@
+package com.test;
+
+public class Child extends Main  {
+    @Override
+    public void exam(){
+        System.out.println("子类");
+    }
+}
---
+++ OOPSenior/src/com/test/Person.java
@@ -0,0 +1,15 @@
+package com.test;
+
+public class Person {
+    public static void  main(String[] args){
+//        Main p1 = new Main();
+//        Main p2 = new Main();
+//        p1.info = "halo";
+//        System.out.println(p2.info);
+//        System.out.println(p1.info);
+//        p1.change();
+
+        Child c1 = new Child();
+        c1.exam();
+    }
+}
